230ea8d92b01a08fb642d6f33b1400538df17fe0
Nicolas Ribeiro <commit@niskii.dev>
2023-02-24 21:43:25 -0300

    First Commit

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Home() {
+	return (
+		<View className="flex-1 bg-background px-8 pt-16">
+			<Text className="text-2xl font-bold text-white">Home</Text>
+		</View>
+	);
+}
